fd039f4a028b5920a43403eaf8411203725874d7
unknown <arthurleao434@gmail.com>
2026-05-04 23:05:02 -0300

    fix porta render

--- server.js
+++ server.js
@@ -1,16 +1,18 @@
 const express = require('express');
+const cors = require('cors');
+
 const app = express();
 
-const cors = require('cors');
 app.use(cors());
-
 app.use(express.json());
 
 app.post('/capi', (req, res) => {
     console.log('Recebeu evento:', req.body);
-    res.sendStatus(200);
+    res.status(200).json({ ok: true });
 });
 
-app.listen(3000, () => {
-    console.log('Servidor rodando...');
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log('Servidor rodando na porta ' + PORT);
 });
